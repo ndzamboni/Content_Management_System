d0d3121df8947efe749452812a885e57e51e4325
ndzamboni <ndzamboni@gmail.com>
2024-07-12 14:46:46 -0400

    added all api routes and the auth.js

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const { Post } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Get all posts for dashboard
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('dashboard', {
+      posts,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Edit post route
+router.get('/edit/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id);
+
+    const post = postData.get({ plain: true });
+
+    res.render('edit-post', {
+      post,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
