c4a5a10132894088870cc65cf3e9fa712f636932
ndzamboni <ndzamboni@gmail.com>
2024-07-12 09:46:43 -0400

    added config.js and connection.js

---
+++ config/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    development: {
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      dialect: 'postgres'
+    },
+    production: {
+      use_env_variable: 'DATABASE_URL',
+      dialect: 'postgres'
+    }
+  };
+  
