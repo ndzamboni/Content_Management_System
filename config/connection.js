747fa58da3d5beac4ddd173aba19b6ce78aefa88
ndzamboni <ndzamboni@gmail.com>
2024-07-14 18:43:28 -0400

    render deploy

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,24 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    port: process.env.DB_PORT,
-  }
-);
+const sequelize = new Sequelize(process.env.DB_URI_INTERNAL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // You may need to set this to true based on your PostgreSQL setup
+    },
+  },
+});
 
 module.exports = sequelize;
+
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
--- test-db-connection.js
+++ test-db-connection.js
@@ -1,16 +1,7 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    port: process.env.DB_PORT,
-  }
-);
+const sequelize = new Sequelize(process.env.DB_URI_INTERNAL);
 
 sequelize.authenticate()
   .then(() => {
